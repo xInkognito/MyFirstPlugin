e7aa3f08c12070cc87235c8a6070df98f8632dfa
xInkognito <inkognita_03@mail.ru>
2024-03-02 11:50:39 +0500

    feat: use TextComponent's

--- src/main/java/me/myplugin/myfirstplugin/commands/FoodCommand.java
+++ src/main/java/me/myplugin/myfirstplugin/commands/FoodCommand.java
@@ -3,8 +3,15 @@ package me.myplugin.myfirstplugin.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import me.myplugin.myfirstplugin.MyFirstPlugin;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import javax.inject.Named;
 
 
 public class FoodCommand extends BaseCommand {
@@ -21,17 +28,31 @@ public class FoodCommand extends BaseCommand {
             String food = plugin.getConfig().getString(player_food);
                 if (food == null)
                 {
-                    player.sendMessage("You didnt set your favorite food yet");
+                    player.sendMessage(Component.text("You didnt set your favorite food yet").color(NamedTextColor.DARK_PURPLE));
                 }
                 else {
-                    player.sendMessage("Your favorite food is " + food);
+                    final TextComponent textComponent = Component
+                            .text("Your favorite food is ")
+                            .color(NamedTextColor.DARK_PURPLE)
+                                    .append(Component
+                                            .text(food)
+                                            .color(NamedTextColor.YELLOW)
+                                            .decorate(TextDecoration.BOLD));
+                    player.sendMessage(textComponent);
                 }
             }
             // If command is "/food <arg>"
             else
             {
                 plugin.getConfig().set(player_food, arg);
-                player.sendMessage("You set your favorite food as " + arg);
+                final TextComponent textComponent2 = Component
+                        .text("You set your favorite food as ")
+                        .color(NamedTextColor.DARK_PURPLE)
+                        .append(Component
+                                .text(arg)
+                                .color(NamedTextColor.YELLOW)
+                                .decorate(TextDecoration.BOLD));
+                player.sendMessage(textComponent2);
                 plugin.saveConfig();
             }
         }
